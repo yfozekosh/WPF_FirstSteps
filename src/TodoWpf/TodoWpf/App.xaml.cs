3549c8f92fa5c206fcc43bac38f4deea75fd1d94
Yurii Fozekosh <yfozekosh@gmail.com>
2021-03-28 19:38:44 +0200

    add PRISM to project

--- src/TodoWpf/TodoWpf/App.xaml.cs
+++ src/TodoWpf/TodoWpf/App.xaml.cs
@@ -1,9 +1,20 @@
-﻿namespace TodoWpf
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using TodoWpf.Views;
+
+namespace TodoWpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+        
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
---
+++ src/TodoWpf/TodoWpf/Views/ShellWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace TodoWpf.Views
+{
+    public partial class ShellWindow : Window
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
